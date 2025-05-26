b2ce96693b8ea194569f6ef29698d6241eef77bb
Jean <jean@DESKTOP-Q42G8FS.cruso.com>
2025-05-26 14:01:31 +0200

    Correct OSM

--- main.dart.js
+++ main.dart.js
@@ -99496,7 +99496,7 @@ a1H(a){return this.c.$1(a)},
 mj(a){return this.d.$0()}}
 A.a19.prototype={
 M(a){var s,r=this,q=null,p=A.bN(10),o=A.a([new A.c2(0,B.a5,A.ae(5,B.h.q()>>>16&255,B.h.q()>>>8&255,B.h.q()&255),B.f,10)],t.V),n=t.p,m=A.aP(A.a([A.ag(B.k.h(0,r.a.f).h(0,"Choix du type de carte"),q,q,q,B.b8,q,q),A.cX(q,q,q,q,B.dY,q,q,new A.azG(r),q,q,q)],n),B.o,B.a7,B.n),l=A.ad(q,q,B.l,B.H,q,q,q,1,q,q,q,q,q,q),k=r.a.e==="OSM",j=k?B.ad:B.ak
-j=A.Pa(B.H,A.ag("Open Street Map",q,q,q,A.ju(q,q,k?B.j:B.h,q,q,q,q,q,q,q,q,q,q,q,q,q,q,!0,q,q,q,q,q,q,q,q),q,q),j,50,new A.azH(r),260)
+j=A.Pa(B.H,A.ag("OpenStreetMap",q,q,q,A.ju(q,q,k?B.j:B.h,q,q,q,q,q,q,q,q,q,q,q,q,q,q,!0,q,q,q,q,q,q,q,q),q,q),j,50,new A.azH(r),260)
 k=r.a.e==="World_Imagery"
 s=k?B.ad:B.ak
 return A.l5(q,A.ad(q,A.eU(B.ab,A.a([B.Ph,new A.c8(B.cy,q,q,A.ad(q,A.bi(A.a([new A.bb(B.hF,m,q),l,B.a0,A.bi(A.a([j,B.a0,A.Pa(B.H,A.ag("ArcGIS - World Imagery",q,q,q,A.ju(q,q,k?B.j:B.h,q,q,q,q,q,q,q,q,q,q,q,q,q,q,!0,q,q,q,q,q,q,q,q),q,q),s,50,new A.azI(r),260)],n),B.o,B.q,B.n),B.a0],n),B.o,B.q,B.n),B.l,q,q,new A.bd(B.j,q,q,p,o,q,B.z),q,q,q,q,q,q,q,300),q)],n),B.y,B.af),B.l,q,q,q,q,q,q,q,B.dM,q,q,340),q,q,q,60,400,q)}}
